43a71b9ac619b1f67dbf126f34fb621cc0c4cd17
Mohammed-Shabeeb <21b448@nssce.ac.in>
2024-03-26 22:46:17 +0530

    community part of home page done

--- unityBid/src/pages/Home.jsx
+++ unityBid/src/pages/Home.jsx
@@ -2,11 +2,40 @@ import React from 'react'
 import Navbar from "../components/Navbar/Navbar"
 import Logo from "../assets/UnityBid-logos_white.png"
 import "./Home.css"
+import Carousel from 'react-multi-carousel';
+import 'react-multi-carousel/lib/styles.css';
+import { COMMUNITIES } from '../components/CommunityList';
+import CommunityCard from '../components/CommunityCard';
 
 const Home = () => {
+
+  const deviceType = window.innerWidth > 1024 ? "desktop" : window.innerWidth > 464 ? "tablet" : "mobile";
+
+  const responsive = {
+    desktop: {
+      breakpoint: { max: 3000, min: 1024 },
+      items: 3,
+      slidesToSlide: 3 // optional, default to 1.
+    },
+    tablet: {
+      breakpoint: { max: 1024, min: 464 },
+      items: 2,
+      slidesToSlide: 2 // optional, default to 1.
+    },
+    mobile: {
+      breakpoint: { max: 464, min: 0 },
+      items: 1,
+      slidesToSlide: 1 // optional, default to 1.
+    }
+  };
+  
+
   return (
     <div className='home-container'>
-      <Navbar />
+      <div className="home-nav">
+        <Navbar />
+      </div>
+      
       <div className="home-home">
           <div className="home-homeName">
               <div className="home-logoContainer">
@@ -16,14 +45,38 @@ const Home = () => {
           <div className="home-homeContent">
               
           </div>
-      </div>
-        Communities
+      </div >
+        
       <div className="home-communities">
+        <h3>Communities</h3>
+        <div className="home-community-content">
+          <Carousel 
+            swipeable={false}
+            draggable={false}
+            showDots={true}
+            responsive={responsive}
+            ssr={true} // means to render carousel on server-side.
+            infinite={true}
+            autoPlaySpeed={1000}
+            keyBoardControl={true}
+            customTransition="all .5"
+            transitionDuration={500}
+            containerClass="carousel-container"
+            removeArrowOnDeviceType={["tablet", "mobile"]}
+            dotListClass="custom-dot-list-style"
+            itemClass="carousel-item-padding-40-px"
+            
+          >
+            {COMMUNITIES.map((community, index) => (
+              <CommunityCard key={index} details={community} />
+            ))}
+          </Carousel>
 
+        </div>
       </div>
-        Auctions
+        
       <div className="home-auctions">
-
+        Auctions
       </div>
     </div>
   )
